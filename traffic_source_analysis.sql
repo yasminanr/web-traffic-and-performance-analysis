18dafddb93232d4e43ad0f87f7fcbe0b02bcaa41
Yasmina Neera <ysmnneera@gmail.com>
2023-02-22 00:17:37 +0700

    Create traffic_source_analysis.sql

---
+++ traffic_source_analysis.sql
@@ -0,0 +1,33 @@
+USE mavenfuzzyfactory;
+
+-- Tables used: website_sessions, website_pageviews, orders
+
+-- Finding Top Traffic Sources
+
+SELECT 
+	utm_source,
+	utm_campaign,
+	http_referer,
+	COUNT(DISTINCT website_session_id) AS sessions_count
+FROM website_sessions
+WHERE created_at < '2012-04-12'
+GROUP BY utm_source, utm_campaign, http_referer
+ORDER BY sessions_count DESC;
+
+-- Traffic Source Conversion Rates
+
+SELECT
+	COUNT(DISTINCT ws.website_session_id) AS sessions_count,
+	COUNT(DISTINCT o.order_id) AS orders,
+	COUNT(DISTINCT o.order_id)/COUNT(DISTINCT ws.website_session_id) AS session_order_conv_rate
+FROM website_sessions ws
+LEFT JOIN orders o 
+	USING (website_session_id)
+WHERE ws.created_at < '2012-04-14'
+	AND ws.utm_source = 'gsearch'
+	AND ws.utm_campaign = 'nonbrand';
+	
+-- Next:
+-- Monitor the impact of bid reductions
+-- Analyze performance trending by device type in order to refine bidding strategy
+
