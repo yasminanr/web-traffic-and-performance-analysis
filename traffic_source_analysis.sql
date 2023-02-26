410b6859afa447c1d174176d2f20efc610e5507f
Yasmina Neera <ysmnneera@gmail.com>
2023-02-27 00:24:47 +0700

    Update traffic_source_analysis.sql

--- traffic_source_analysis.sql
+++ traffic_source_analysis.sql
@@ -37,3 +37,18 @@ WHERE created_at < '2012-05-10'
 	AND utm_source = 'gsearch'
 	AND utm_campaign = 'nonbrand'
 GROUP BY WEEK(created_at);
+
+-- Bid Optimization for Paid Traffic
+
+SELECT 	
+	ws.device_type,
+	COUNT(DISTINCT ws.website_session_id) AS sessions_count,
+	COUNT(DISTINCT o.order_id) AS orders,
+	COUNT(DISTINCT o.order_id)/COUNT(DISTINCT ws.website_session_id) AS session_order_conv_rate
+FROM website_sessions ws 
+LEFT JOIN orders o 
+	USING(website_session_id)
+WHERE ws.created_at < '2012-05-11'
+	AND utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand'
+GROUP BY ws.device_type;
