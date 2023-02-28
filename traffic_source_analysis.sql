246a8be7d80a8ee53f3a656eeafdce4d42df3156
Yasmina Neera <ysmnneera@gmail.com>
2023-03-01 01:54:41 +0700

    Add query for weekly traffic trend by device type

--- traffic_source_analysis.sql
+++ traffic_source_analysis.sql
@@ -52,3 +52,16 @@ WHERE ws.created_at < '2012-05-11'
 	AND utm_source = 'gsearch'
 	AND utm_campaign = 'nonbrand'
 GROUP BY ws.device_type;
+
+-- Trending with Granular Segments
+
+SELECT 
+	MIN(DATE(created_at)) AS week_start_date,
+	COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS desktop_sessions,
+	COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mobile_sessions
+FROM website_sessions 
+WHERE created_at BETWEEN '2012-04-15' AND '2012-06-09'
+	AND utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand'
+GROUP BY WEEK(created_at);
+
