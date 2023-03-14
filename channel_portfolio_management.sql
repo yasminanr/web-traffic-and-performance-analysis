b8902a20d74ce86653239d3d614a0e803eb1e73e
Yasmina Neera <ysmnneera@gmail.com>
2023-03-14 18:09:30 +0700

    Add query comparing channel characteristics

--- channel_portfolio_management.sql
+++ channel_portfolio_management.sql
@@ -10,3 +10,15 @@ FROM website_sessions
 WHERE created_at BETWEEN '2012-08-22' AND '2012-11-29'
 	AND utm_campaign = 'nonbrand'
 GROUP BY WEEK(created_at);
+
+-- Comparing Channel Characteristics
+
+SELECT 
+	utm_source,
+	COUNT(DISTINCT website_session_id) AS sessions_count,
+	COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mobile_sessions,
+	COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END)/COUNT(DISTINCT website_session_id) AS mobile_pct
+FROM website_sessions 
+WHERE created_at BETWEEN '2012-08-22' AND '2012-11-30'
+	AND utm_campaign = 'nonbrand'
+GROUP BY utm_source;
