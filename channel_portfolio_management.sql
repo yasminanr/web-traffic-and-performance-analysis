5064bc30b45c63b0a2e3246a03e2e993328fa2ab
Yasmina Neera <ysmnneera@gmail.com>
2023-03-21 23:22:06 +0700

    Add query for bid optimization

--- channel_portfolio_management.sql
+++ channel_portfolio_management.sql
@@ -22,3 +22,18 @@ FROM website_sessions
 WHERE created_at BETWEEN '2012-08-22' AND '2012-11-30'
 	AND utm_campaign = 'nonbrand'
 GROUP BY utm_source;
+
+-- Cross-Channel Bid Optimization
+
+SELECT 
+	ws.device_type,
+	ws.utm_source,
+	COUNT(DISTINCT ws.website_session_id) AS sessions_count,
+	COUNT(DISTINCT o.order_id) AS order_count,
+	COUNT(DISTINCT o.order_id)/COUNT(DISTINCT ws.website_session_id) AS session_order_conv_rate
+FROM website_sessions ws 
+LEFT JOIN orders o 
+	USING (website_session_id)
+WHERE ws.created_at BETWEEN '2012-08-22' AND '2012-09-19'
+	AND utm_campaign = 'nonbrand'
+GROUP BY ws.device_type, ws.utm_source;
